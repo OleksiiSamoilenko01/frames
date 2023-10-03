32f8acf1c690b2e350c9410dad9e048729a4bb43
oleksij <o.samoilenko95@gmail.com>
2023-10-03 18:19:52 +0300

    dataprovider update

--- src/test/java/framesTest/FrameTest.java
+++ src/test/java/framesTest/FrameTest.java
@@ -1,33 +1,29 @@
 package framesTest;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 
 public class FrameTest extends BaseTestClass {
     String URL = "https://the-internet.herokuapp.com/nested_frames";
 
-    @Test
-    public void allFrames(){
+    @DataProvider
+    public Object[][] frameData() {
+        return new Object[][] {
+                {"frame-left", "LEFT"},
+                {"frame-middle", "MIDDLE"},
+                {"frame-right", "RIGHT"}
+        };
+    }
+
+    @Test(dataProvider = "frameData" )
+    public void allFrames(String frame, String expectedResult){
         driver.get(URL);
         driver.switchTo().frame("frame-top");
-        driver.switchTo().frame("frame-left");
+        driver.switchTo().frame(frame);
 
-        String body = driver.findElement(By.xpath("//body[contains(text(),\"LEFT\")]")).getText();
-        Assert.assertEquals(body, "LEFT");
-
-        driver.switchTo().defaultContent();
-        driver.switchTo().frame("frame-top");
-        driver.switchTo().frame("frame-middle");
-
-        String middle = driver.findElement(By.xpath("//body/div[@id = \"content\"]")).getText();
-        Assert.assertEquals(middle, "MIDDLE");
-
-        driver.switchTo().defaultContent();
-        driver.switchTo().frame("frame-top");
-        driver.switchTo().frame("frame-right");
-
-        String right = driver.findElement(By.xpath("//body[contains(text(),\"RIGHT\")]")).getText();
-        Assert.assertEquals(right, "RIGHT");
+        String body = driver.findElement(By.xpath("//body")).getText();
+        Assert.assertEquals(body, expectedResult);
 
     }
 
